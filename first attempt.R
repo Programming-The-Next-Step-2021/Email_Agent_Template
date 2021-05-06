7b7c2a8fcf8b7d83f50ea823319cf66641eb6ce6
paulaottens <75449930+paulaottens@users.noreply.github.com>
2021-05-06 19:09:28 +0200

    second commit

---
+++ first attempt.R
@@ -0,0 +1,14 @@
+
+#' Create new Template
+#'
+#' @param x some data
+#' @param y some other data
+#'
+#' @return new template
+#' @export
+#'
+#' @examples
+#' newTemplate <- createTemplate(data)
+createTemplate<-function(x,y){
+  x+y
+}
