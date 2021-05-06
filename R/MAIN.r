c085d73e5da7fb05385601fae4fcd5955ed5f001
paulaottens <75449930+paulaottens@users.noreply.github.com>
2021-05-06 19:19:37 +0200

    commit

---
+++ R/MAIN.r
@@ -0,0 +1,16 @@
+
+
+
+#' Create Template
+#'
+#' @param x SOME DATA
+#' @param y SOME OTHER DATA
+#'
+#' @return The newly created template
+#' @export
+#'
+#' @examples
+#' newtemplate <- createTemplate(sampletemplate)
+createTemplate <- function(x,y){
+  x+y
+}
