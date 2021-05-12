fd4b1029f1111a5de9778d2b48f6748262a753be
paulaottens <75449930+paulaottens@users.noreply.github.com>
2021-05-13 01:21:36 +0200

    included a link to my drop box account to enable saving and loading output from my APP

--- R/emailtemplateapp.R
+++ R/emailtemplateapp.R
@@ -1,5 +1,48 @@
 library(shiny)
 library(tidyverse)
+library(rdrop2)
+
+# Define the fields we want to save from the form
+fields <- c("type", "topic","ending", "greeting","name")
+
+
+# in any drop_* function, pass `dtoken = token
+# Tokens are valid until revoked.
+
+outputDir <- "Email Templates"
+
+saveData <- function(data) {
+  
+  data <- t(data)
+  
+  # Create a unique file name
+  
+  fileName <- sprintf("%s_%s.csv", as.integer(Sys.time()), digest::digest(data))
+  
+  # Write the data to a temporary file locally
+  
+  filePath <- file.path(tempdir(), fileName)
+  
+  write.csv(data, filePath, row.names = FALSE, quote = TRUE)
+  # Upload the file to Dropbox
+  
+  drop_upload(filePath, path = outputDir, dtoken = token)
+  
+}
+
+loadData <- function() {
+  
+  # Read all the files into a list
+  filesInfo <- drop_dir(outputDir, dtoken = token)
+  
+  filePaths <- filesInfo$path_display
+  
+  data <- lapply(filePaths, drop_read_csv, stringsAsFactors = FALSE)
+  
+  # Concatenate all data together into one data.frame
+  data <- do.call(rbind, data)
+  data
+}
 
 #requirements
   #select response type 
@@ -23,43 +66,75 @@ ui <- fluidPage(
   tags$link(rel = "stylesheet",type = "button/css", href="bootstrap.min.css")),
   titlePanel("Custom Email Templates"),
   sidebarLayout(
+    
     sidebarPanel(
+      
       selectInput("type","Choose a response type:",
                   c("first_response"="first",
                     "reply"="reply",
                     "post_phone"="phone",
                     "update"="update")),
+      
       selectInput("topic","Choose a topic:",
                   list('post delivery/user phase'= list("damage","productcomplaint","invoice","return"),
                        'delivery phase'= list("where is my parcel","returned by courier"))),
+      
       selectInput("ending","Choose an ending:",
-                  list('Monday'= list("morning","afternoon","evening"),
-                       'Tuesday'= list("morning","afternoon","evening"),
-                       'Wednesday'= list("morning","afternoon","evening"),
-                       'Thursday'= list("morning","afternoon","evening"),
-                       'Friday'= list("morning","afternoon","evening"),
-                       'Saturday'= list("morning","afternoon","evening"),
-                       'Sunday'= list("morning","afternoon","evening"))),
+                  list(`Monday`= list("morning","afternoon","evening"),
+                       `Tuesday`= list("morning","afternoon","evening"),
+                       `Wednesday`= list("morning","afternoon","evening"),
+                       `Thursday`= list("morning","afternoon","evening"),
+                       `Friday`= list("morning","afternoon","evening"),
+                       `Saturday`= list("morning","afternoon","evening"),
+                       `Sunday`= list("morning","afternoon","evening"))
+                  ),
+      
       textInput("Greeting","Type a greeting:","kind regards,"),
       textInput("Name","Type your Name:"),
-      submitButton("Update Template", icon("refresh"))
-      
+      actionButton("submit", "Submit")
+     
     ),
+    
     mainPanel(
       textOutput("type"),
       textOutput("topic"),
       textOutput("ending"),
       textOutput("greeting"),
-      textOutput("name")
+      textOutput("name"),
+      
     ),
   )
 )
 
-server <- function(input, output){
+server <- function(input, output, session){
+  
   output$type<-renderText({input$type})
+  
   output$topic<-renderText({input$topic})
+  
   output$ending<-renderText({input$ending})
+  
   output$greeting<-renderText({input$Greeting})
+  
   output$name<-renderText({input$Name})
+  
+  # Whenever a field is filled, aggregate all form data
+  formData <- reactive({
+    data <- sapply(fields, function(x) input[[x]])
+    data
+  })
+  
+  # When the Submit button is clicked, save the form data
+  observeEvent(input$submit, {
+    saveData(formData())
+  })
+  
+  # Show the previous responses
+  # (update with current response when Submit is clicked)
+  output$responses <- DT::renderDataTable({
+    input$submit
+    loadData()
+  }) 
+ 
 }
 shinyApp(ui=ui,server = server)
