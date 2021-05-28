feb2b0cdfdce19af55c5ce0cc6c676b543e39f18
paulaottens <75449930+paulaottens@users.noreply.github.com>
2021-05-28 04:03:37 +0200

    deleted some unnecessary code

--- R/dropbox token.R
+++ R/dropbox token.R
@@ -4,14 +4,3 @@ token <- drop_auth()
 saveRDS(token, file = "token.rds")
 
 
-
-library(dplyr)
-drop_acc() %>% data.frame()
-
-list('Monday'= list("morning","afternoon","evening"),
-     'Tuesday'= list("morning","afternoon","evening"),
-     'Wednesday'= list("morning","afternoon","evening"),
-     'Thursday'= list("morning","afternoon","evening"),
-     'Friday'= list("morning","afternoon","evening"),
-     'Saturday'= list("morning","afternoon","evening"),
-     'Sunday'= list("morning","afternoon","evening"))),
