0f343ab0088a3073e47c2db4d01d7688b8975e86
paulaottens <75449930+paulaottens@users.noreply.github.com>
2021-05-30 22:36:38 +0200

    unit testing

--- tests/testthat/test-app.R
+++ tests/testthat/test-app.R
@@ -2,23 +2,23 @@ library(shiny)
 library(testthat)
 library(shinytest)
 
+#testing shiny app
+context('core Email Template functionality')
+
+#open shiny app
 app <- ShinyDriver$new("~/UVA/programming next step/Email_Agent_Template/tests/app")
 
+#get all values
 vals <- app$getAllValues()
 
 str(vals)
 
-
-
-library(EmailTemplate) 
-
-context('core Email Template functionality')
 
 test_that('functionality of load data function',{
   expect_identical(vals$export, read.csv("~/UVA/programming next step/Email_Agent_Template/expected_output.csv"))
 })
 
 
-
-
+#stop shiny app
+app$stop
 
